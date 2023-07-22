09f1640de77a306d67f0f550e42a440cbb1d0f74
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-07-22 20:50:31 +0500

    3rd task

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,34 @@
 public class Main {
 
-    public static int checkArray(int[] mas, int value, int min){
+    public static int sumArray(int[][] mas){
 
-        if(mas == null) return -3;
-        if(mas.length < min) return -1;
-
+        if(mas.length != mas[0].length){
+            throw new RuntimeException("The array isn't square " + mas.length + " " + mas[0].length);
+        }
+        int sum = 0;
         for(int i = 0; i < mas.length; i++){
-            if (mas[i] == value){
-                return i;
+            for(int j = 0; j < mas[0].length; j++){
+                if(mas[i][j] != 0 && mas[i][j] != 1){
+                    throw new RuntimeException("Incorrect values " + mas[i][j] + " at indexes: " + i + ":" + j);
+                }
+                sum += mas[i][j];
             }
         }
-        return -2;
+        return sum;
     }
-    public static String errorText(int exeption){
-        String text = switch (exeption) {
-            case -1 -> "the length is shorter than the minimum stated";
-            case -2 -> "not found";
-            case -3 -> "null instead of the array";
-            default -> "the element index = " + exeption;
-        };
-        return text;
-    }
+//    public static String errorText(int exeption){
+//        String text = switch (exeption) {
+//            case -1 -> "the length is shorter than the minimum stated";
+//            case -2 -> "not found";
+//            case -3 -> "null instead of the array";
+//            default -> "the element index = " + exeption;
+//        };
+//        return text;
+//    }
 
     public static void main(String[] args) {
-        int[] arr = {4,5,0,17,-4};
-        int min_length = 7;
-        int value = 5;
+        int[][] arr = {{1, 0}, {5, 0}};
 
-        System.out.println(errorText(checkArray(arr, value, min_length)));
+        System.out.println(sumArray(arr));
     }
 }
